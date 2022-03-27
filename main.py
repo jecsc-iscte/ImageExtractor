e47fbdbeb96e9e8b50097d07d32376319139c33f
jecsc-iscte <81762317+jecsct@users.noreply.github.com>
2022-03-27 02:12:00 +0100

    Updated GUI & extra features

--- main.py
+++ main.py
@@ -13,59 +13,83 @@ from docx.shared import Cm
 def main():
     gui = tk.Tk()
 
-    label1 = tk.Label(gui, text="Localizacao no sistema:")
-    label2 = tk.Label(gui, text="Capitulo:")
-    label3 = tk.Label(gui, text="Exemplo : C:\\Users\\Joao\\Documents\\Berserk")
-    label4 = tk.Label(gui, text="")
-    label1.grid(row=0, column=0, sticky=tk.W, pady=2)
-    label2.grid(row=3, column=0, sticky=tk.W, pady=2)
-    label3.grid(row=1, column=0, sticky=tk.W, pady=2)
-    label4.grid(row=2, column=0, sticky=tk.W, pady=2)
+    webLbl = tk.Label(gui, text="Website Url:")
+    locationLbl = tk.Label(gui, text="Save Location:")
+    infoLabel = tk.Label(gui, text="")
+    fileNameLbl = tk.Label(gui, text="FileName:")
+    webLbl.grid(row=0, column=0, sticky=tk.W, pady=2)
+    locationLbl.grid(row=1, column=0, sticky=tk.W, pady=2)
+    fileNameLbl.grid(row=2, column=0, sticky=tk.W, pady=2)
+    infoLabel.grid(row=3, column=0, sticky=tk.W, pady=2)
 
-    entry1 = tk.Entry(gui)
-    entry1.config(width=50)
-    entry2 = tk.Entry(gui)
-    entry1.grid(row=0, column=1, pady=2)
-    entry2.grid(row=3, column=1, pady=2)
+    webEntry = tk.Entry(gui)
+    webEntry.config(width=50)
+    locationEntry = tk.Entry(gui)
+    locationEntry.config(width=50)
+    fileNameEntry = tk.Entry(gui)
+    fileNameEntry.config(width=15)
+    webEntry.grid(row=0, column=1, pady=2)
+    locationEntry.grid(row=1, column=1, pady=2)
+    fileNameEntry.grid(row=2, column=1, sticky=tk.W, pady=2)
 
-    button = tk.Button(gui, text="Generate", command=lambda: imageExtractor(entry1.get(), entry2.get(), button, label4))
-    button.grid(row=4, column=0, pady=2)
+    convert = tk.IntVar(value=1)
+    checkBox = tk.Checkbutton(gui, text='Convert to PDF', variable=convert, onvalue=1, offvalue=0, )
+    checkBox.grid(row=4, column=0, sticky=tk.W, pady=2)
+
+    clear = tk.IntVar(value=1)
+    clearBox = tk.Checkbutton(gui, text='Clear Directory Previous Images', variable=clear, onvalue=1, offvalue=0)
+    clearBox.grid(row=4, column=1, sticky=tk.W, pady=2)
+
+    generateBtn = tk.Button(gui, text="Generate",
+                            command=lambda: imageExtractor(webEntry.get(), locationEntry.get(), fileNameEntry.get(),
+                                                           infoLabel,
+                                                           generateBtn,
+                                                           convert, clear))
+    generateBtn.grid(row=5, column=1, pady=2)
 
     gui.mainloop()
 
-def imageExtractor(folderPATH, chapter, button, label):
-    changeButtonState(button, "disable")
+
+def imageExtractor(url, folderPATH, fileName, infoLabel, generateBtn, convert, clear):
+    changeButtonState(generateBtn, "disable")
     if not os.path.isdir(folderPATH):
-        editLabelText(label, "O diretorio nao existe")
-    elif not websiteExists(chapter):
-        editLabelText(label, "Esse capitulo nao existe")
+        editLabelText(infoLabel, "O diretorio nao existe")
+    elif not websiteExists(url):
+        editLabelText(infoLabel, "Esse capitulo nao existe")
     else:
         os.chdir(folderPATH)
-        editLabelText(label, "Clean das imagens desnecessarias")
-        removeDirFiles('jpg', folderPATH, chapter)
-        editLabelText(label, "Download das imagens necessarias")
-        downloadImages('http://berserkmanga.net/manga/berserk-chapter-'+chapter+'/')
-        editLabelText(label, "A criar word")
-        addImagesToWord(folderPATH, chapter)
-        editLabelText(label, "A converter word para pdf")
-        wordToPDF(folderPATH, chapter)
-        editLabelText(label, "Feito!")
-        removeDirFiles('jpg', folderPATH, chapter)
-        editLabelText(label, "")
-    changeButtonState(button, "normal")
+        if clear:
+            editLabelText(infoLabel, "Clean das imagens desnecessarias")
+            removeDirFiles('jpg', folderPATH, fileName)
+            # editLabelText(label, "Download das imagens necessarias")
+        downloadImages(url)
+        if convert:
+            # editLabelText(label, "A criar word")
+            addImagesToWord(folderPATH, fileName)
+            # editLabelText(label, "A converter word para pdf")
+            wordToPDF(folderPATH, fileName)
+            # editLabelText(label, "Feito!")
+        if clear:
+            removeDirFiles('jpg', folderPATH, fileName)
+        editLabelText(infoLabel, "")
+    changeButtonState(generateBtn, "normal")
+
 
 def changeButtonState(button, state):
     button["state"] = state
 
-def websiteExists(chapter):
-    response = requests.get('http://berserkmanga.net/manga/berserk-chapter-'+chapter+'/')
+
+def websiteExists(url):
+    response = requests.get(url)
     if response.status_code == 200:
         return True
     else:
         return False
 
+
 def editLabelText(label, newText):
     label.config(text=newText, highlightcolor="red")
+
 
 def downloadImages(url):
     r = requests.get(url)
@@ -80,10 +104,11 @@ def downloadImages(url):
             f.write(im.content)
         i = i + 1
 
-def addImagesToWord(folderPATH, chapter):
+
+def addImagesToWord(folderPATH, fileName):
     document = Document()
-    updateMargins(document, folderPATH, chapter)
-    document = Document(os.path.join(folderPATH, 'capitulo' + chapter + '.docx'))
+    updateMargins(document, folderPATH, fileName)
+    document = Document(os.path.join(folderPATH, fileName + '.docx'))
     for path in glob.glob1(folderPATH, 'tempBerserk*.jpg'):
         img = Image.open(os.path.join(folderPATH, path))
         if img.size[0] > 1500:
@@ -95,19 +120,21 @@ def addImagesToWord(folderPATH, chapter):
         p = document.add_paragraph()
         r = p.add_run()
         r.add_picture(os.path.join(folderPATH, path))
-    document.save(os.path.join(folderPATH, 'capitulo' + chapter + '.docx'))
+    document.save(os.path.join(folderPATH, fileName + '.docx'))
 
-def updateMargins(document, folderPATH, chapter):
+
+def updateMargins(document, folderPATH, fileName):
     sections = document.sections
     for section in sections:
         section.top_margin = Cm(0)
         section.left_margin = Cm(0.8)
 
-    document.save(os.path.join(folderPATH, 'capitulo' + chapter + '.docx'))
+    document.save(os.path.join(folderPATH, fileName + '.docx'))
 
-def wordToPDF(folderPATH, chapter):
-    in_file = os.path.abspath(os.path.join(folderPATH, 'capitulo' + chapter + '.docx'))
-    out_file = os.path.abspath(os.path.join(folderPATH, 'capitulo' + chapter + '.pdf'))
+
+def wordToPDF(folderPATH, fileName):
+    in_file = os.path.abspath(os.path.join(folderPATH, fileName + '.docx'))
+    out_file = os.path.abspath(os.path.join(folderPATH, fileName + '.pdf'))
 
     wdFormatPDF = 17
 
@@ -117,11 +144,13 @@ def wordToPDF(folderPATH, chapter):
     doc.Close()
     word.Quit()
 
-def removeDirFiles(fileType, folderPATH, chapter):
-    for path in glob.glob1(folderPATH, 'tempBerserk*.'+fileType):
+
+def removeDirFiles(fileType, folderPATH, fileName):
+    for path in glob.glob1(folderPATH, 'tempBerserk*.' + fileType):
         os.remove(os.path.join(folderPATH, path))
-    if os.path.isfile(os.path.join(folderPATH, 'capitulo' + chapter + '.docx')):
-        os.remove(os.path.join(folderPATH, 'capitulo' + chapter + '.docx'))
+    if os.path.isfile(os.path.join(folderPATH, fileName + '.docx')):
+        os.remove(os.path.join(folderPATH, fileName + '.docx'))
+
 
 if __name__ == '__main__':
     main()
